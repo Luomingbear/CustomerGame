cd698039fc9dec8b023542c8b9028eacd5334231
luoming <luomingbear@163.com>
2021-08-22 11:01:58 +0800

    默认打开开始界面

--- Scences/Start/Start.gd
+++ Scences/Start/Start.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-
+func _ready():
+	RoleFactory.init()
 
 func _on_NewBtn_button_up():
 	ArchiveManager.createArchive()
-	get_tree().change_scene("res://Scences/World.tscn")
+	get_tree().change_scene("res://Scences/World/World.tscn")
 
 
 func _on_ContinueBtn_button_up():
 	ArchiveManager.loadArchive()	
-	get_tree().change_scene("res://Scences/World.tscn")
+	get_tree().change_scene("res://Scences/World/World.tscn")
 	
