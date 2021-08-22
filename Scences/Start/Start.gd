2e79b7118cb6a1f59b7b910d7b9c70c36ddbc371
luoming <luomingbear@163.com>
2021-08-22 11:09:54 +0800

    添加是否有缓存的判读

--- Scences/Start/Start.gd
+++ Scences/Start/Start.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+onready var continueBtn = $Control/ContinueBtn
+
 func _ready():
 	RoleFactory.init()
+	if ArchiveManager.hasArchive():
+		continueBtn.visible = true
+	else:
+		continueBtn.visible = false
 
 func _on_NewBtn_button_up():
 	ArchiveManager.createArchive()
+	
 	get_tree().change_scene("res://Scences/World/World.tscn")
 
 
